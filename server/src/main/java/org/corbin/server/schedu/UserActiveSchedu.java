edb7f3546d7e50b9f4edfaf69865d00c82526391
yin <corbin_yin@163.com>
2019-05-23 13:22:15 +0800

    1。添加banner 2.修复自动下线 3.修改演示时定时任务时间

--- server/src/main/java/org/corbin/server/schedu/UserActiveSchedu.java
+++ server/src/main/java/org/corbin/server/schedu/UserActiveSchedu.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
 import java.util.List;
 
 @Component
@@ -16,9 +15,53 @@ import java.util.List;
 public class UserActiveSchedu {
     /**
      * 用户登录误操作在线时长
-     * 单位：min
+     * 单位：ms
      */
-    private final static Integer LOGIN_STATUS_NO_ACTION_TIME=30;
+    //实验时为5min
+    //private final static Long LOGIN_STATUS_NO_ACTION_TIME=30*60*1000L;
+    private final static Long LOGIN_STATUS_NO_ACTION_TIME=5*60*1000L;
+
+    private String pkq="       quu..__\n" +
+            "         $$$b  `---.__\n" +
+            "          \"$$b        `--.                          ___.---uuudP\n" +
+            "           `$$b           `.__.------.__     __.---'      $$$$\"              .\n" +
+            "             \"$b          -'            `-.-'            $$$\"              .'|\n" +
+            "               \".                                       d$\"             _.'  |\n" +
+            "                 `.   /                              ...\"             .'     |\n" +
+            "                   `./                           ..::-'            _.'       |\n" +
+            "                    /                         .:::-'            .-'         .'\n" +
+            "                   :                          ::''\\          _.'            |\n" +
+            "                  .' .-.             .-.           `.      .'               |\n" +
+            "                  : /'$$|           .@\"$\\           `.   .'              _.-'\n" +
+            "                 .'|$u$$|          |$$,$$|           |  <            _.-'\n" +
+            "                 | `:$$:'          :$$$$$:           `.  `.       .-'\n" +
+            "                 :                  `\"--'             |    `-.     \\\n" +
+            "                :##.       ==             .###.       `.      `.    `\\\n" +
+            "                |##:                      :###:        |        >     >\n" +
+            "                |#'     `..'`..'          `###'        x:      /     /\n" +
+            "                 \\                                   xXX|     /    ./\n" +
+            "                  \\                                xXXX'|    /   ./\n" +
+            "                  /`-.                                  `.  /   /\n" +
+            "                 :    `-  ...........,                   | /  .'\n" +
+            "                 |         ``:::::::'       .            |<    `.\n" +
+            "                 |             ```          |           x| \\ `.:``.\n" +
+            "                 |                         .'    /'   xXX|  `:`M`M':.\n" +
+            "                 |    |                    ;    /:' xXXX'|  -'MMMMM:'\n" +
+            "                 `.  .'                   :    /:'       |-'MMMM.-'\n" +
+            "                  |  |                   .'   /'        .'MMM.-'\n" +
+            "                  `'`'                   :  ,'          |MMM<\n" +
+            "                    |                     `'            |tbap\\\n" +
+            "                     \\                                  :MM.-'\n" +
+            "                      \\                 |              .''\n" +
+            "                       \\.               `.            /\n" +
+            "                        /     .:::::::.. :           /\n" +
+            "                       |     .:::::::::::`.         /\n" +
+            "                       |   .:::------------\\       /\n" +
+            "                      /   .''               >::'  /\n" +
+            "                      `',:                 :    .'\n" +
+            "                                           `:.:'\n" +
+            "\n" ;
+
 
     @Autowired
     private UserActiveInfoService userActiveInfoService;
@@ -39,9 +82,12 @@ public class UserActiveSchedu {
         log.info("执行用户自动下线策略");
         //用户操作
         for (UserActiveInfo userActiveInfo : userActiveInfoList) {
-            if ((new Date()).getTime()-userActiveInfo.getUpdateTime().getTime()-LOGIN_STATUS_NO_ACTION_TIME*1000>0){
+            //超时时间
+            long overTime=(System.currentTimeMillis()-userActiveInfo.getUpdateTime().getTime()-LOGIN_STATUS_NO_ACTION_TIME);
+            if (overTime>0){
                 userActiveInfoService.deleteUserActive(userActiveInfo.getUserId());
-                log.info("用户id为"+userActiveInfo.getUserId()+"已自动下线");
+                log.info(pkq);
+                log.info("用户id为"+userActiveInfo.getUserId()+"已自动下线"+"超时时间:"+overTime);
             }
         }
 
