74daf6243710a76cb037e4e2da946314e2cd7169
xiesu <corbin_yin@163.com>
2019-08-27 22:14:32 +0800

    1.修改数据库链接,添加allowPublicKeyRetrieval=true,修复Public Key Retrieval is not allowed 错误(不允许公钥检索) 2.修复缺少的搜索vo

---
+++ client/src/main/java/org/corbin/client/vo/search/SearchSongVo.java
@@ -0,0 +1,9 @@
+package org.corbin.client.vo.search;
+
+import lombok.Data;
+import org.corbin.common.base.vo.BaseVo;
+
+@Data
+public class SearchSongVo extends BaseVo {
+    private String searchValue;
+}
